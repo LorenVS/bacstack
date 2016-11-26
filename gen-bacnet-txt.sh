06c57ee8418e0f45e091f470a64318cebe6c0ffb
Loren Van Spronsen <lorenvs@outlook.com>
2016-11-25 16:39:38 -0800

    Update bacnet.txt to the ASHRAE Standard 135-2012

--- gen-bacnet-txt.sh
+++ gen-bacnet-txt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pdftotext -x 0 -y 36 -W 612 -H 684 -f 402 -l 443 -layout -nopgbrk bacnet-standard.pdf bacnet.txt
+pdftotext -x 0 -y 36 -W 612 -H 684 -f 653 -l 725 -layout -nopgbrk bacnet-standard.pdf bacnet.txt
 cat bacnet.txt \
   | sed "s/[*][*][*][*].*$//g" \
   | sed "s/--.*$//g" \
